34e4638957dd72473c0f4a7fe5477c2be7b28e30
gamyac12 <gamyachanda@gmail.com>
2026-02-12 00:24:48 +0530

    hi

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lj$=lskh^#*tjs6ave-@v5lrbo=+myvzt!@dp#d(opc=a4ng#e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.232.185.170', 'localhost', '127.0.0.1']
 
 
 # Application definition
